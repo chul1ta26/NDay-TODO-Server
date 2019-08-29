61d9f2d91fc8f8814783583eda3b8c6a4b686f4e
chul1ta26 <dayhany26@gmail.com>
2019-08-29 10:57:09 -0400

    initial commit

---
+++ src/main/java/com/nury/ndaytodo/NdaytodoApplication.java
@@ -0,0 +1,13 @@
+package com.nury.ndaytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdaytodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdaytodoApplication.class, args);
+	}
+
+}
